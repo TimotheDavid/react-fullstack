1db9b2ff5d26a095049d47ec09ff46118b45bec6
TimotheDavid <timoth.david@gmail.com>
2020-09-13 23:42:17 +0200

    finish 1.1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = ({course}) => (
+  <h1>{course}</h1>
+)
+
+const Content = ({content,part}) => (
+    <p>
+      {part} {content}
+    </p>
+)
+
+const Total = ({total}) => (
+  <p>Number of exercises {total}</p>
+)
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <div>
+    <Header course={course}/>
+    <Content content={exercises1} part={part1}/>
+    <Content content={exercises2} part={part2}/>
+    <Content content={exercises3} part={part3}/>
+    <Total total={exercises1 + exercises2 + exercises3}/>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
