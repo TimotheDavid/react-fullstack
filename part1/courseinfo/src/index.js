7d1f9a2074d52afdeea5bd002704409eaa6525dc
TimotheDavid <timoth.david@gmail.com>
2020-09-14 09:56:43 +0200

    change to array

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,28 +19,41 @@ const Total = ({total}) => (
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+   const sum = (array) => {
+    let result  = 0;
+    for(var i =0; i<array.length; i++){
+      result += array[i].exercises; 
+      
+    }
+    return result
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  
+  const result = sum(parts)
+
   return(
-
-
     <div>
     <Header course={course}/>
-    <Content content={part1}/>
-    <Content content={part2}/>
-    <Content content={part3}/>
-    <Total total={part1.exercises + part2.exercises + part3.exercises}/>
-
+    <Content content={parts[0]}/>
+    <Content content={parts[1]}/>
+    <Content content={parts[2]}/>
+    <Total total={result}/>
+  
+  
     </div>
   )
 }
